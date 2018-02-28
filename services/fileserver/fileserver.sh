7f403be1bb35031d5c74bc787c4b30e31b3886ed
Gareth Murphy <garethcmurphy@gmail.com>
2018-02-28 11:23:40 +0100

    add helm charts for file download

--- services/fileserver/fileserver.sh
+++ services/fileserver/fileserver.sh
@@ -1,3 +1,11 @@
+envarray=(dev)
+
+
+echo $1
+
+   export LOCAL_ENV="${envarray[i]}"
+   echo $LOCAL_ENV
+helm del --purge fileserver
 cd services/fileserver/
    if [ -d "./minitornado/" ]; then
 	cd minitornado
@@ -7,7 +15,17 @@ git clone https://github.com/garethcmurphy/minitornado.git
 	cd minitornado
    fi
 cd tornado
-docker build . -t garethcmurphy/tornado
+export FILESERVER_IMAGE_VERSION=$(git rev-parse HEAD)
+docker build . -t garethcmurphy/tornado:$FILESERVER_IMAGE_VERSION$LOCAL_ENV
+docker push garethcmurphy/tornado:$FILESERVER_IMAGE_VERSION$LOCAL_ENV
+echo "Deploying to Kubernetes"
+cd ..
+cd ..
+cd ..
+pwd
+echo helm install fileserver --name fileserver --namespace $LOCAL_ENV --set image.tag=$FILESERVER_IMAGE_VERSION$LOCAL_ENV --set image.repository=garethcmurphy/tornado
+helm install fileserver --name fileserver --namespace $LOCAL_ENV --set image.tag=$FILESERVER_IMAGE_VERSION$LOCAL_ENV --set image.repository=garethcmurphy/tornado
+# envsubst < ../catanie-deployment.yaml | kubectl apply -f - --validate=false
 
 
 
