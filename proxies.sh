01f55c4f4aebd5f537a7f8d992ee19c40ed4d3e2
garethcmurphy <garethcmurphy@gmail.com>
2018-03-07 18:02:27 +0100

    add catamel proxy

--- proxies.sh
+++ proxies.sh
@@ -8,4 +8,5 @@ kubectl port-forward --namespace dev $(kubectl get po -n dev | grep mongodb | aw
 kubectl port-forward --namespace dev $(kubectl get po -n dev | grep rabbitmq | awk '{print $1;}') 15672:15672 5672:5672
 kubectl port-forward --namespace dev $(kubectl get po -n dev | grep catanie | awk '{print $3;}') 8000:80
 kubectl port-forward --namespace dev $(kubectl get po -n dev | grep node | awk '{print $3;}') 1880:1880
+kubectl port-forward --namespace dev $(kubectl get po -n dev | grep catamel | awk '{print $1;}') 3000:3000
 docker login localhost:5000
