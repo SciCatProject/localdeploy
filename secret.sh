80ffcb575968df607de4e6e46960e170d326c034
Gareth Murphy <garethcmurphy@gmail.com>
2018-08-27 11:38:47 +0200

    add dmscprod namespace to secrets

--- secret.sh
+++ secret.sh
@@ -9,7 +9,7 @@ if [ "$(hostname)" == "kubetest01.dm.esss.dk" ]; then
 
 	kubectl create secret -ndmsc tls catanieservice --key catanie.key --cert catanie.crt
 	kubectl create secret -ndev tls catamelservice --key catamel.key --cert catamel.crt
-	kubectl create secret -ndev tls landingserverservice --key landingserver.key --cert landingserver.crt
+	kubectl create secret -ndmsc tls landingserverservice --key landingserver.key --cert landingserver.crt
 	kubectl create secret -ndev tls fileserverservice --key fileserver.key --cert fileserver.crt
 elif  [ "$(hostname)" == "scicat01.esss.lu.se" ]; then
 	openssl req -x509 -newkey rsa:4096 -sha256 -nodes -keyout catanie.key -out catanie.crt -subj "/CN=scicat01.esss.lu.se" -days 3650
