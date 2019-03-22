7742fd16ba92494e7ba33a9b4604b081dcc6b00d
Gareth Murphy <garethcmurphy@gmail.com>
2019-03-22 14:11:12 +0100

    Update secret.sh

--- secret.sh
+++ secret.sh
@@ -25,7 +25,7 @@ elif  [ "$(hostname)" == "scicat01.esss.lu.se" ]; then
 	kubectl create secret -ness tls landingserverservice --key landingserver.key --cert landingserver.crt
 	kubectl create secret -ndev tls catamelservice --key catamel.key --cert catamel.crt
 	kubectl create secret -ndev tls fileserverservice --key fileserver.key --cert fileserver.crt
-elif  [ "$(hostname)" == "k8-lrg-prod.esss.dk" ]; then
+elif  [ "$(hostname)" == "k8-lrg-serv-prod.esss.dk" ]; then
     kubectl create ns dmscprod
 	openssl req -x509 -newkey rsa:4096 -sha256 -nodes -keyout catanie.key -out catanie.crt -subj "/CN=scicat.esss.dk" -days 3650
 	openssl req -x509 -newkey rsa:4096 -sha256 -nodes -keyout catamel.key -out catamel.crt -subj "/CN=scicatapi.esss.dk" -days 3650
