4fa5d16ef8d09b9bda3b3fd4ce63f6ed3e9d466b
Gareth Murphy <garethcmurphy@gmail.com>
2019-07-24 11:59:47 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,17 +6,13 @@ elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
         LOCAL_IP=`hostname --ip-address`
 fi
 echo $LOCAL_IP
-#minikube start -v7  --insecure-registry localhost:5000 --extra-config=apiserver.GenericServerRunOptions.AuthorizationMode=RBAC
 
 minikube start 
-#kubectl config use-context minikube #should auto set, but added in case
 
 kubectl -n kube-system create sa tiller
 kubectl create -f rbac-config.yaml
 helm init --service-account tiller
 helm repo update
-#kubectl apply -f ./deployments/registry.yaml
-#kubectl apply -f ./deployments/ingress/nginx-controller.yaml
 
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/static/mandatory.yaml
 
