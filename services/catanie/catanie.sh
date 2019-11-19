3b719635895b24c98765175416565695ddb37e62
Luke Gorman <lukegorman@gmail.com>
2019-11-19 14:37:01 +0100

    heml upgrade with namespace environement

--- services/catanie/catanie.sh
+++ services/catanie/catanie.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
-envarray=(dmsc)
+if [ "$#" -ne 1 ]; then
+  echo "Usage ./catanie.sh ENVIRONMENT" >&2
+  exit 1
+fi
+export env=$1
+
+#envarray=(dmsc)
 export REPO=https://github.com/SciCatProject/catanie.git
 
 INGRESS_NAME=" "
@@ -15,38 +21,39 @@ if [ "$(hostname)" == "kubetest01.dm.esss.dk" ]; then
 fi
 
 
-echo $1
+#echo $1
 
-for ((i=0;i<${#envarray[@]};i++)); do
-    export LOCAL_ENV="${envarray[i]}"
-    export LOCAL_IP="$1"
-    echo $LOCAL_ENV
-    helm del --purge catanie
-    cd ./services/catanie/
-    if [ -d "./component/" ]; then
-        cd component/
-        git checkout develop
-        git pull
-        ./CI/ESS/copyimages.sh
-    else
-        git clone $REPO component
-        cd component/
-        git checkout develop
-        git pull
-        ./CI/ESS/copyimages.sh
-    fi
-    export CATANIE_IMAGE_VERSION=$(git rev-parse HEAD)
-    echo "Deploying to Kubernetes"
-    cd ..
-    
-    function docker_tag_exists() {
-        curl --silent -f -lSL https://index.docker.io/v1/repositories/$1/tags/$2 > /dev/null
-    }
-    
-    if docker_tag_exists dacat/catanie latest; then
-        echo exists
-        helm install dacat-gui --name catanie --namespace $LOCAL_ENV --set image.tag=latest --set image.repository=$2 ${INGRESS_NAME}
-    else
-        echo not exists
-    fi
-done
+#for ((i=0;i<${#envarray[@]};i++)); do
+#export LOCAL_ENV="${envarray[i]}"
+#export LOCAL_IP="$1"
+#echo $LOCAL_ENV
+#helm del --purge catanie
+cd ./services/catanie/
+if [ -d "./component/" ]; then
+    cd component/
+    git checkout develop
+    git pull
+    ./CI/ESS/copyimages.sh
+else
+    git clone $REPO component
+    cd component/
+    git checkout develop
+    git pull
+    ./CI/ESS/copyimages.sh
+fi
+export tag=$(git rev-parse HEAD)
+echo "Deploying to Kubernetes"
+cd ..
+
+function docker_tag_exists() {
+    curl --silent -f -lSL https://index.docker.io/v1/repositories/$1/tags/$2 > /dev/null
+}
+
+if docker_tag_exists dacat/catanie latest; then
+    echo exists
+    helm upgrade catanie-${env} dacat-gui --wait --recreate-pods --namespace=${env} --set image.tag=$tag$env
+    helm history catanie-${env}
+else
+    echo not exists
+fi
+#done
