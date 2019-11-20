032b6159515aedd0d112a162fc08f32256229a65
Gareth Murphy <garethcmurphy@gmail.com>
2019-11-20 10:36:54 +0100

    add tag

--- init.sh
+++ init.sh
@@ -2,6 +2,10 @@
 env="dev"
 cd services/catanie
 
+cd component
+export tag=$(git rev-parse HEAD)
+cd ..
+
 helm install --name=catanie-${env} dacat-gui  --namespace=${env} --set image.tag=$tag$env --wait
 
 cd ../..
