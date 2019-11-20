0b9e39cd40fb24e429ad089b2e1649db11a954db
Gareth Murphy <garethcmurphy@gmail.com>
2019-11-20 10:29:02 +0100

    re-add env

--- run.sh
+++ run.sh
@@ -6,21 +6,21 @@ kubectl delete -f rabbit.yaml
 NS_DIR=./namespaces/*.yaml
 
 if [ "$(uname)" == "Darwin" ]; then
-        LOCAL_IP=`ipconfig getifaddr en0`
-elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-        LOCAL_IP=`hostname --ip-address`
+    LOCAL_IP=`ipconfig getifaddr en0`
+    elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    LOCAL_IP=`hostname --ip-address`
 fi
 
 DOCKER_REPO="localhost:5000"
-KAFKA=0 
+KAFKA=0
 
 while getopts 'fhkd:' flag; do
-  case "${flag}" in
-    d) DOCKER_REPO=${OPTARG} ;;
-    h) echo "-d for Docker Repo prefix"; exit 1 ;;
-    k) KAFKA=1 ;;
-    f) FILESERVER=1 ;;
-  esac
+    case "${flag}" in
+        d) DOCKER_REPO=${OPTARG} ;;
+        h) echo "-d for Docker Repo prefix"; exit 1 ;;
+        k) KAFKA=1 ;;
+        f) FILESERVER=1 ;;
+    esac
 done
 
 CATANIE_REPO=$DOCKER_REPO/catanie
@@ -33,27 +33,25 @@ helm del --purge local-mongodb
 helm del --purge local-postgresql
 helm del --purge local-rabbit
 helm del --purge local-node
-        if [[ "KAFKA" -eq "1" ]]; then
-helm del --purge local-kafka
+if [[ "KAFKA" -eq "1" ]]; then
+    helm del --purge local-kafka
 fi
 
 for file in $NS_DIR; do
-  f="$(basename $file)"
-  ns="${f%.*}"
-  kubectl create -f $file
-  export LOCAL_ENV="$ns"
-kubectl apply -f mongo.yaml
-  helm install stable/mongodb --version 0.4.15 --namespace $LOCAL_ENV --name local-mongodb
-kubectl apply -f postgres.yaml
-  helm install stable/postgresql  --namespace $LOCAL_ENV --name local-postgresql
-        if [[ "KAFKA" -eq "1" ]]; then
-
-helm repo add incubator http://storage.googleapis.com/kubernetes-charts-incubator
-  helm install --name local-kafka incubator/kafka --namespace $LOCAL_ENV 
-fi
-kubectl apply -f rabbit.yaml
-  helm install stable/rabbitmq --version 0.6.3 --namespace $LOCAL_ENV --name local-rabbit --set rabbitmqUsername=admin,rabbitmqPassword=admin
-  helm install services/node-red --namespace dev --name local-node
+    f="$(basename $file)"
+    ns="${f%.*}"
+    kubectl create -f $file
+    export LOCAL_ENV="$ns"
+    kubectl apply -f mongo.yaml
+    helm install stable/mongodb --version 0.4.15 --namespace $LOCAL_ENV --name local-mongodb
+    if [[ "KAFKA" -eq "1" ]]; then
+        
+        helm repo add incubator http://storage.googleapis.com/kubernetes-charts-incubator
+        helm install --name local-kafka incubator/kafka --namespace $LOCAL_ENV
+    fi
+    kubectl apply -f rabbit.yaml
+    helm install stable/rabbitmq --version 0.6.3 --namespace $LOCAL_ENV --name local-rabbit --set rabbitmqUsername=admin,rabbitmqPassword=admin
+    helm install services/node-red --namespace dev --name local-node
 done
 ./secret.sh
 
@@ -62,12 +60,12 @@ done
 SERVICES_DIR=./services/*/*.sh
 
 for file in $SERVICES_DIR; do
-  #bash $file $LOCAL_IP $CATANIE_REPO $CATAMEL_REPO
+    #bash $file $LOCAL_IP $CATANIE_REPO $CATAMEL_REPO
     if [ $file !=  "./services/fileserver/fileserver.sh" ]; then
         bash $file $LOCAL_IP $CATANIE_REPO $CATAMEL_REPO
     else
         if [[ "FILESERVER" -eq "1" ]]; then
-        bash $file $LOCAL_IP $CATANIE_REPO $CATAMEL_REPO
+            bash $file $LOCAL_IP $CATANIE_REPO $CATAMEL_REPO
         fi
     fi
 done
