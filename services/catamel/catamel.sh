587f45b6da5f361705376eb9730ce74e3c6fb9c7
Luke Gorman <lukegorman@gmail.com>
2019-11-21 10:39:32 +0100

    rename params

--- services/catamel/catamel.sh
+++ services/catamel/catamel.sh
@@ -32,7 +32,7 @@ function docker_tag_exists() {
     curl --silent -f -lSL https://index.docker.io/v1/repositories/$1/tags/$2 > /dev/null
 }
 
-if docker_tag_exists dacat/catamel $CATAMEL_IMAGE_VERSION$LOCAL_ENV; then
+if docker_tag_exists dacat/catamel $tag$env; then
     echo exists
     #helm install dacat-api-server --name catamel --namespace $LOCAL_ENV --set image.tag=$CATAMEL_IMAGE_VERSION$LOCAL_ENV --set image.repository=$3 ${INGRESS_NAME}
     helm upgrade dacat-api-server-${env} dacat-api-server --namespace=${env} --set image.tag=$tag
