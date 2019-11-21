8618c0cb0086470bd2184afebd12b01f28ad28d9
Gareth Murphy <garethcmurphy@gmail.com>
2019-11-21 10:20:57 +0100

    add mongo init

---
+++ mongoinit.sh
@@ -0,0 +1,5 @@
+    envarray=( production qa)
+for ((i=0;i<${#envarray[@]};i++)); do
+    export env="${envarray[i]}"
+    helm install stable/mongodb  --namespace $env --name mongodb-$env --set persistence.enabled=false
+don
