845fda4dbe4a2b2da3ebc12b6719e847945f0197
Gareth Murphy <garethcmurphy@gmail.com>
2019-11-22 11:20:47 +0100

    downgrade mongo

--- mongoinit.sh
+++ mongoinit.sh
@@ -2,5 +2,5 @@
 envarray=( dev production qa)
 for ((i=0;i<${#envarray[@]};i++)); do
     export env="${envarray[i]}"
-    helm install stable/mongodb  --namespace $env --name mongodb-$env --set persistence.enabled=false
+    helm install stable/mongodb --version 0.4.15  --namespace $env --name mongodb-$env --set persistence.enabled=false
 done
