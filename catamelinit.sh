8699b4c22d428abad6ed9ed1a59cd9fed9b6a7d8
Gareth Murphy <garethcmurphy@gmail.com>
2019-11-22 10:52:55 +0100

    add delete catamel to init

--- catamelinit.sh
+++ catamelinit.sh
@@ -15,6 +15,7 @@ envarray=(dev production qa)
 envarray=(dev qa)
 for ((i=0;i<${#envarray[@]};i++)); do
     export env="${envarray[i]}"
+    helm del --purge catamel-${env}
     helm install --name=catamel-${env} dacat-api-server  --namespace=${env} --set image.tag=$tag$env --wait 
 done
 
